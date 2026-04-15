cca17dbfcc41277d9daafb512204d604a10debfa
katocollins <ckato131@gmail.com>
2026-04-16 00:22:22 +0300

    Fix

--- submission/04.sh
+++ submission/04.sh
@@ -7,7 +7,7 @@
 publicKey=02e3af28965693b9ce1228f9d468149b831d6a0540b25e8a9900f71372c11fb277
 timestamp=1495584032
 
-lehex=$(submission/integer2lehex.sh $timestamp | grep "Hexcode" | awk '{print $2}')
+lehex=$(submission/int2lehex.sh $timestamp | grep "Hexcode" | awk '{print $2}')
 
 pubKeyHash=$(echo -n $publicKey | xxd -r -p | openssl dgst -sha256 -binary | openssl dgst -rmd160 | awk '{print $2}')
 
