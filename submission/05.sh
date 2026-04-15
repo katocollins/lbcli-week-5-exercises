b1da1f51e17e9d5fa2873247cc5f6a0a566e61d7
katocollins <ckato131@gmail.com>
2026-04-16 00:31:50 +0300

    Submission of Week 5 scripts

--- submission/05.sh
+++ submission/05.sh
@@ -3,7 +3,7 @@
 publicKey=02e3af28965693b9ce1228f9d468149b831d6a0540b25e8a9900f71372c11fb277
 blocks=150
 
-# Convert to little-endian hex (minimal bytes)
+# Convert to hex
 hex=$(printf '%x' $blocks)
 
 # Ensure even length
@@ -11,10 +11,18 @@ if [ $(( ${#hex} % 2 )) -ne 0 ]; then
   hex="0$hex"
 fi
 
+# If MSB is set, append 00
+first_byte=${hex:0:2}
+first_byte_dec=$((16#$first_byte))
+
+if (( first_byte_dec >= 128 )); then
+  hex="${hex}00"
+fi
+
 # Convert to little endian
 lehex=$(echo $hex | sed 's/\(..\)/\1 /g' | awk '{for(i=NF;i>0;i--) printf $i}')
 
-# Get byte length (for push opcode)
+# Push opcode
 bytes=$((${#lehex} / 2))
 push=$(printf '%02x' $bytes)
 
@@ -27,5 +35,5 @@ pubKeyHash=$(echo -n $publicKey | xxd -r -p \
 # Build script
 result="${push}${lehex}b27576a914${pubKeyHash}88ac"
 
-# Clean output
+# Output clean
 printf "%s" "$result"
