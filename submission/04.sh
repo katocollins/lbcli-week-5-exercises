d0cf2b6cbed4fb752daae5a136a50990d8a93a99
katocollins <ckato131@gmail.com>
2026-04-16 00:27:24 +0300

    Submission of Week 5 scripts

--- submission/04.sh
+++ submission/04.sh
@@ -1,14 +1,19 @@
-# Create a CLTV script with a timestamp of 1495584032 and public key below:
-# publicKey=02e3af28965693b9ce1228f9d468149b831d6a0540b25e8a9900f71372c11fb277
-
-
-#!/bin/bash
+#!/usr/bin/env bash
 
 publicKey=02e3af28965693b9ce1228f9d468149b831d6a0540b25e8a9900f71372c11fb277
 timestamp=1495584032
 
-lehex=$(submission/int2lehex.sh $timestamp | grep "Hexcode" | awk '{print $2}')
+# Convert timestamp to 4-byte little endian hex
+lehex=$(printf '%08x' $timestamp | sed 's/\(..\)/\1 /g' | awk '{for(i=NF;i>0;i--) printf $i}')
 
-pubKeyHash=$(echo -n $publicKey | xxd -r -p | openssl dgst -sha256 -binary | openssl dgst -rmd160 | awk '{print $2}')
+# Hash160(pubkey)
+pubKeyHash=$(echo -n $publicKey | xxd -r -p \
+  | openssl dgst -sha256 -binary \
+  | openssl dgst -rmd160 \
+  | awk '{print $2}')
 
-echo "${lehex}b17576a914${pubKeyHash}88ac"
+# Construct script
+result="04${lehex}b17576a914${pubKeyHash}88ac"
+
+# Output clean (NO newline issues)
+printf "%s" "$result"
