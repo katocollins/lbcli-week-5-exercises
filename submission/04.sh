4a5881d2be921181e362fa97b4a83302e72d5d98
katocollins <ckato131@gmail.com>
2026-04-16 00:14:52 +0300

    Fix

--- submission/04.sh
+++ submission/04.sh
@@ -1,6 +1,8 @@
 # Create a CLTV script with a timestamp of 1495584032 and public key below:
-publicKey=02e3af28965693b9ce1228f9d468149b831d6a0540b25e8a9900f71372c11fb277
+# publicKey=02e3af28965693b9ce1228f9d468149b831d6a0540b25e8a9900f71372c11fb277
 
+
+publicKey=02e3af28965693b9ce1228f9d468149b831d6a0540b25e8a9900f71372c11fb277
 timestamp=1495584032
 
 lehex=$(submission/integer2lehex.sh $timestamp | grep "Hexcode" | awk '{print $2}')
